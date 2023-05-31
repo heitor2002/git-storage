9391beeaa58c1c98e2a15ad47d18514979071ed6
heitor2002 <montagnini.heitor@gmail.com>
2023-05-31 13:46:58 -0300

    style: estilização página de adicionar rep.

--- src/pages/adicionar-repositorio.jsx
+++ src/pages/adicionar-repositorio.jsx
@@ -1,19 +1,26 @@
 import { Layout } from "@/components/Layout";
 
-export default function AddNewRepo(){
-    return(
-        <>
-        <Layout>
-            <div className="form-new-repo">
-                <form>
-                    <input type="text" placeholder="Adicionar repositório:"/>
-                    <fieldset>
-                        <input type="checkbox" name="" id="" />
-                        <input type="checkbox" name="" id="" />
-                    </fieldset>
-                </form>
-            </div>
-        </Layout>
-        </>
-    )
+export default function AddNewRepo() {
+  return (
+    <>
+      <Layout>
+        <div className="form-new-repo">
+          <form>
+            <input type="text" placeholder="Adicionar repositório:" />
+            <fieldset>
+              <div>
+                <input type="checkbox" name="" id="" />
+                <label>Público</label>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="" />
+                <label>Privado</label>
+              </div>
+            </fieldset>
+            <input type="submit" />
+          </form>
+        </div>
+      </Layout>
+    </>
+  );
 }
