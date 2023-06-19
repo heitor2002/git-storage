3712dcd71e13e871b6bccb65f134096f205aa7d0
heitor2002 <montagnini.heitor@gmail.com>
2023-06-19 19:43:51 -0300

    feat: altera navegação quando logado.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,25 @@
+import { getCookie } from "cookies-next";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export const Navigation = () => {
+  const [activeProfile, setActiveProfile] = useState(false)
+  const cookieToken = getCookie("authorization");
+
+  useEffect(() => {
+    if(cookieToken){
+      setActiveProfile(true)
+    }
+  }, [])
+
   return (
     <>
       <nav className="navigation-bar">
         <Link href={"/"}>Repositório Público</Link>
         <Link href={"/private"}>Repositório Privado</Link>
         <Link href={"/adicionar-repositorio"}>Adicionar projeto</Link>
-        <Link href={"/login"}>Entrar / Registrar</Link>
+        {!activeProfile && <Link href={"/login"}>Entrar / Registrar</Link>}
+        {activeProfile && <Link href={"/profile"}>Meu perfil</Link>}
       </nav>
     </>
   );
